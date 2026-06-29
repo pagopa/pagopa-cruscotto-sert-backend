63eb8391ea9d336c3ce41d213cd82fafd4f3cd69
mamicoacc <m.amico@accenture.com>
2026-06-29 16:24:39 +0200

    restore apim

--- infra/04_apim_api.tf
+++ infra/04_apim_api.tf
@@ -1,14 +1,8 @@
 locals {
   repo_name = "pagopa-cruscotto-sert-backend"
 
-  display_name_sert = "Cruscotto Sert pagoPA backend service API"
-  description_sert = "Cruscotto Sert pagoPA backend service API"
-  display_name_management = "Cruscotto Sert management pagoPA backend service API"
-  description_management = "Cruscotto Sert management pagoPA backend service API"
-  display_name_auth = "Cruscotto Sert AUTH pagoPA backend service API"
-  description_auth = "Cruscotto Sert AUTH pagoPA backend service API"
-
-
+  display_name = "Cruscotto Sert pagoPA backend service API"
+  description = "Cruscotto Sert pagoPA backend service API"
   path  = "smo/cruscotto-sert"
 
   host         = "api.${var.apim_dns_zone_prefix}.${var.external_domain}"
@@ -19,19 +13,19 @@ resource "azurerm_api_management_group" "api_group" {
   name                = local.apim.product_id
   resource_group_name = local.apim.rg
   api_management_name = local.apim.name
-  display_name        = local.display_name_sert
-  description         = local.description_sert
+  display_name        = local.display_name
+  description         = local.description
 }
 
 resource "azurerm_api_management_api_version_set" "api_version_set" {
   name                = format("%s-${local.repo_name}", var.env_short)
   resource_group_name = local.apim.rg
   api_management_name = local.apim.name
-  display_name        = local.display_name_sert
+  display_name        = local.display_name
   versioning_scheme   = "Segment"
 }
 
-module "api_sert" {
+module "api_v1" {
   source = "git::https://github.com/pagopa/terraform-azurerm-v3.git//api_management_api?ref=v8.62.1"
 
   name                  = format("%s-${local.repo_name}", var.env_short)
@@ -43,74 +37,15 @@ module "api_sert" {
   version_set_id = azurerm_api_management_api_version_set.api_version_set.id
   api_version    = "v1"
 
-  description  = local.description_sert
-  display_name = local.display_name_sert
+  description  = local.description
+  display_name = local.display_name
   path         = local.path
   protocols    = ["https"]
 
   service_url = null
 
   content_format = "openapi"
-  content_value  = templatefile("../openapi/openapi_sert.json", {
-    host = local.host
-  })
-
-  xml_content = templatefile("./policy/_base_policy.xml", {
-    hostname = var.hostname
-  })
-}
-
-module "api_auth" {
-  source = "git::https://github.com/pagopa/terraform-azurerm-v3.git//api_management_api?ref=v8.62.1"
-
-  name                  = format("%s-${local.repo_name}", var.env_short)
-  api_management_name   = local.apim.name
-  resource_group_name   = local.apim.rg
-  product_ids           = [local.apim.product_id]
-  subscription_required = false # via JWT
-
-  version_set_id = azurerm_api_management_api_version_set.api_version_set.id
-  api_version    = "v1"
-
-  description  = local.description_auth
-  display_name = local.display_name_auth
-  path         = local.path
-  protocols    = ["https"]
-
-  service_url = null
-
-  content_format = "openapi"
-  content_value  = templatefile("../openapi/openapi_auth.json", {
-    host = local.host
-  })
-
-  xml_content = templatefile("./policy/_base_policy.xml", {
-    hostname = var.hostname
-  })
-}
-
-
-module "api_management" {
-  source = "git::https://github.com/pagopa/terraform-azurerm-v3.git//api_management_api?ref=v8.62.1"
-
-  name                  = format("%s-${local.repo_name}", var.env_short)
-  api_management_name   = local.apim.name
-  resource_group_name   = local.apim.rg
-  product_ids           = [local.apim.product_id]
-  subscription_required = false # via JWT
-
-  version_set_id = azurerm_api_management_api_version_set.api_version_set.id
-  api_version    = "v1"
-
-  description  = local.description_management
-  display_name = local.display_name_management
-  path         = local.path
-  protocols    = ["https"]
-
-  service_url = null
-
-  content_format = "openapi"
-  content_value  = templatefile("../openapi/openapi_management.json", {
+  content_value  = templatefile("../openapi/openapi.json", {
     host = local.host
   })
 
