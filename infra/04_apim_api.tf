90549e007ccc18c779de69a3963018c351030017
leonardo.cenzi <leonardo.cenzi@external.nexigroup.com>
2026-06-30 16:34:09 +0200

    first try fix deploy

--- infra/04_apim_api.tf
+++ infra/04_apim_api.tf
@@ -1,78 +1,57 @@
-module "api_sert" {
-  source = "git::https://github.com/pagopa/terraform-azurerm-v3.git//api_management_api?ref=v8.62.1"
+locals {
+  repo_name = "pagopa-cruscotto-sert-backend"
 
-  name = format("%s-%s-sert", var.env_short, local.repo_name)
+  host     = "api.${var.apim_dns_zone_prefix}.${var.external_domain}"
+  hostname = var.hostname
 
-  api_management_name = local.apim.name
-  resource_group_name = local.apim.rg
+  apis = {
+    sert = {
+      display_name = "Cruscotto Sert pagoPA backend service API"
+      description  = "Cruscotto Sert pagoPA backend service API"
+      path         = "smo/cruscotto-sert"
+      openapi      = "../openapi/openapi_sert.json"
+    }
 
-  product_ids           = [local.apim.product_id]
-  subscription_required = false
+    management = {
+      display_name = "Cruscotto Management pagoPA backend service API"
+      description  = "Cruscotto Management pagoPA backend service API"
+      path         = "smo/cruscotto-management"
+      openapi      = "../openapi/openapi_management.json"
+    }
 
-  version_set_id = azurerm_api_management_api_version_set.api_version_set.id
-  api_version    = "v1"
-
-  display_name = local.apis.sert.display_name
-  description  = local.apis.sert.description
-
-  path = local.apis.sert.path
-
-  protocols = ["https"]
-  service_url = null
-
-  content_format = "openapi"
-
-  content_value = templatefile(local.apis.sert.openapi, {
-    host = local.host
-  })
-
-  xml_content = templatefile("./policy/_base_policy.xml", {
-    hostname = local.hostname
-  })
+    auth = {
+      display_name = "Cruscotto AUTH pagoPA backend service API"
+      description  = "Cruscotto AUTH pagoPA backend service API"
+      path         = "smo/cruscotto-auth"
+      openapi      = "../openapi/openapi_auth.json"
+    }
+  }
 }
 
-module "api_management" {
-  depends_on = [module.api_sert]
-
-  source = "git::https://github.com/pagopa/terraform-azurerm-v3.git//api_management_api?ref=v8.62.1"
-
-  name = format("%s-%s-management", var.env_short, local.repo_name)
-
-  api_management_name = local.apim.name
+resource "azurerm_api_management_group" "api_group" {
+  name                = local.apim.product_id
   resource_group_name = local.apim.rg
+  api_management_name = local.apim.name
 
-  product_ids           = [local.apim.product_id]
-  subscription_required = false
-
-  version_set_id = azurerm_api_management_api_version_set.api_version_set.id
-  api_version    = "v1"
-
-  display_name = local.apis.management.display_name
-  description  = local.apis.management.description
-
-  path = local.apis.management.path
-
-  protocols = ["https"]
-  service_url = null
-
-  content_format = "openapi"
-
-  content_value = templatefile(local.apis.management.openapi, {
-    host = local.host
-  })
-
-  xml_content = templatefile("./policy/_base_policy.xml", {
-    hostname = local.hostname
-  })
+  display_name = "Cruscotto Sert APIs"
+  description  = "Group for Cruscotto Sert APIs"
 }
 
-module "api_auth" {
-  depends_on = [module.api_management]
+resource "azurerm_api_management_api_version_set" "api_version_set" {
+  name                = format("%s-%s", var.env_short, local.repo_name)
+  resource_group_name = local.apim.rg
+  api_management_name = local.apim.name
+
+  display_name      = "Cruscotto Sert pagoPA backend service API"
+  versioning_scheme = "Segment"
+}
+
+module "apis" {
+  for_each = local.apis
 
   source = "git::https://github.com/pagopa/terraform-azurerm-v3.git//api_management_api?ref=v8.62.1"
 
-  name = format("%s-%s-auth", var.env_short, local.repo_name)
-
+  name                = format("%s-%s-%s", var.env_short, local.repo_name, each.key)
   api_management_name = local.apim.name
   resource_group_name = local.apim.rg
 
@@ -82,17 +61,17 @@ module "api_auth" {
   version_set_id = azurerm_api_management_api_version_set.api_version_set.id
   api_version    = "v1"
 
-  display_name = local.apis.auth.display_name
-  description  = local.apis.auth.description
+  display_name = each.value.display_name
+  description  = each.value.description
 
-  path = local.apis.auth.path
-
+  path      = each.value.path
   protocols = ["https"]
+
   service_url = null
 
   content_format = "openapi"
 
-  content_value = templatefile(local.apis.auth.openapi, {
+  content_value = templatefile(each.value.openapi, {
     host = local.host
   })
 
